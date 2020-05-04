552903ccd3eb9dd4168f984fe419a45150cc48b0
kensuke goto <kensuke.goto.1981@gmail.com>
2020-05-04 22:35:24 +0900

    追加:proto.jsでロジックのプロト作成（ネット切れたため途中）

---
+++ app/proto.js
@@ -0,0 +1,26 @@
+const { getJson,getHTML } = require("./modules/getPages");
+
+(async () => {
+  const data = await getJson("https://www3.nhk.or.jp/news/json16/syuyo.json");
+  const domain = "https://www3.nhk.or.jp/news/";
+  const links = data.channel.item.map(item => item.link);
+  const processes = links.map(link => {
+    return new Promise(async resolve => {
+
+      const html = await getHTML(domain + link);
+      new Promise(resolve2 => {
+        resolve2("yeah");
+      })
+      .then(txt => {
+        resolve(txt);
+      })
+
+    })
+  })
+
+  Promise.all(processes).then(data => {
+    console.log(data);
+  })
+
+})();
+
