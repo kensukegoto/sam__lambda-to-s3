50925f7199114a6fc238efaa1076cfc40c98dbbf
kensuke goto <kensuke.goto.1981@gmail.com>
2020-05-04 23:52:30 +0900

    追加:JSONの一覧内のhtmlを取得し保存

--- app/app.js
+++ app/app.js
@@ -8,27 +8,36 @@ const saveInS3 = require("./modules/saveInS3");
 exports.handler = async (event, context, callback) => {
 
   const data = await getJson("https://www3.nhk.or.jp/news/json16/syuyo.json");
-  const item = data.channel.item[0];
-  const link = item.link;
   const domain = "https://www3.nhk.or.jp/news/";
 
-  const html = await getHTML(domain + link);
+  const links = data.channel.item.map(item => item.link);
 
-  const BucketName = process.env.BucketName;
+  const processes = links.map(link => {
 
-  const params = {
-    Bucket: BucketName,
-    Key: link,
-    Body: html,
-    ContentType: "html"
-  };
+    return new Promise(async resolve => {
+
+      const html = await getHTML(domain + link);
+      const BucketName = process.env.BucketName;
+      const params = {
+        Bucket: BucketName,
+        Key: link,
+        Body: html,
+        ContentType: "html"
+      };
+    
+      await saveInS3(params);
+      
+      resolve();
+
+    })
+  })
 
 
-  await saveInS3(params);
-
-  callback(null,{
-    statusCode: 200,
-    body: JSON.stringify({message:"OK?"})
+  await Promise.all(processes).then(data => {
+    callback(null,{
+      statusCode: 200,
+      body: JSON.stringify({message:"OK"})
+    })
   })
 
 };
